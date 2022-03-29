faa42a7b7d809958e307ac85afd7780d1cc325a6
shoukou-lee <lee_seungho@inha.edu>
2022-03-29 22:42:36 +0900

    add: simple proxy example

---
+++ src/test/java/com/shoukou/whymytransactionalnotwork/proxy/MailFormatterTest.java
@@ -0,0 +1,23 @@
+package com.shoukou.whymytransactionalnotwork.proxy;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MailFormatterTest {
+
+    @Test
+    void simpleProxyUpperCaseMailFormatterTest() {
+        MailFormatter targetFormatter = new SimpleMailFormatter();
+        MailFormatter proxyFormatter = new SimpleProxyUpperCaseMailFormatter(targetFormatter);
+        String content = "abc";
+
+        String withHeader = proxyFormatter.appendHeader(content);
+        assertThat(withHeader).isEqualTo("[HEADER] ABC");
+
+        String withSignature = proxyFormatter.appendSignature(content);
+        assertThat(withSignature).isEqualTo("ABC - SHOUKOU LEE");
+    }
+
+}
